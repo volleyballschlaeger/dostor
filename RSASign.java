9d4371e17bb154c20bbf89bef9e8899b93845f43
ruelle <ruelle@ruelle-System-Product-Name.(none)>
2015-02-28 18:15:46 +0100

    Added RSA signature

---
+++ RSASign.java
@@ -0,0 +1,45 @@
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+//import android.util.Base64;
+
+public class RSASign {
+	private PrivateKey privateKey = null;
+
+	RSASign( String privateKeyStr )
+	{
+		try {
+			byte[] privateKeyBytes = new BASE64Decoder().decodeBuffer( privateKeyStr );
+			KeySpec privateKeySpec = new PKCS8EncodedKeySpec( privateKeyBytes );
+			KeyFactory keyFactory = KeyFactory.getInstance( "RSA" );
+			privateKey = keyFactory.generatePrivate( privateKeySpec );
+		}
+		catch ( Exception e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String sign( byte[] data )
+	{
+		if( privateKey == null )
+			return null;
+		String result = null;
+		try {
+			Signature sig = Signature.getInstance( "MD5WithRSA" );
+			sig.initSign( privateKey );
+			sig.update(data);
+			byte[] sigBytes = sig.sign();
+			result = new BASE64Encoder().encode( sigBytes );
+		} catch ( Exception e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
