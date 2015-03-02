1d7139a8123d639f85ab7fff3bf2470ff13c5623
ruelle <ruelle@ruelle-HP-625.(none)>
2015-03-02 19:19:01 +0100

    Added a simple db cursor

---
+++ RowList.java
@@ -0,0 +1,28 @@
+/**
+ * A simplified database cursor.
+ */
+
+public interface RowList {
+	/**
+	 * Moves to the first position.
+	 * @return Wether the move succeeded.
+	 */
+	public boolean first();
+
+	/**
+	 * Moves to the next position.
+	 * @return Wether the move succeeded.
+	 */
+	public boolean next();
+
+	/**
+	 * @return Number of columns.
+	 */
+	public int getColumnCount();
+
+	/**
+	 * @param columnIndex The zero-based index of the target column.
+	 * @return The value of that column.
+	 */
+	public String getString( int columnIndex );
+}
