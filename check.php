02c4de4497778d982ec24ed6f815c9c97cf7cba9
ruelle <ruelle@ruelle-HP-625.(none)>
2015-05-16 16:38:54 +0200

    Added id check

---
+++ check.php
@@ -0,0 +1,12 @@
+<?php
+function checkId( $id, $data )
+{
+	if( strlen( $id ) != 64 )
+		return FALSE;
+	if( !ctype_xdigit( $id ) )
+		return FALSE;
+	$md5a = substr( $id, 0, 32 );
+	$md5b = md5( $data );
+	return $md5a == $md5b;
+}
+?>
