ddfe6c003fb342e5d29070a13b2c77a1095c53dd
ruelle <ruelle@ruelle-HP-625.(none)>
2015-07-21 19:43:18 +0200

    Added RowListOut

--- Http.java
+++ Http.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -28,6 +29,29 @@ public class Http {
 		finally {
 			if( reader != null )
 				reader.close();
+		}
+	}
+
+	public static void BinRowListOut( OutputStream out, RowList rl ) throws IOException
+	{
+		int colcount = rl.getColumnCount();
+		boolean run = rl.first();
+
+		while( run )
+		{
+			StringBuilder sb = new StringBuilder();
+			for( int i = 0; i < colcount - 1; i++ )
+				sb.append( rl.getString( i ) + "\r\n" );
+			byte[] head = sb.toString().getBytes();
+			byte[] tail = rl.getBinary( colcount - 1 );
+			run = rl.next();
+			if( tail == null )
+				continue;
+			int len = head.length + tail.length;
+			out.write( ( "start:" + len + "\r\n" ).getBytes() );
+			out.write( head );
+			out.write( tail );
+			out.write( "\r\n".getBytes() );
 		}
 	}
 }
